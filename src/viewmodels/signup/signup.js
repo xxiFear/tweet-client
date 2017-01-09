9189e39e06bbba866694ed8b1fcf0b180101c7d4
xxiFear <matthias.nord@hotmail.de>
2017-01-09 18:14:53 +0100

    Added validation to register form

--- src/viewmodels/signup/signup.js
+++ src/viewmodels/signup/signup.js
@@ -1,21 +1,93 @@
 import {inject} from 'aurelia-framework';
-import DonationService from '../../services/tweet-service';
+import TweetService from '../../services/tweet-service';
 
-@inject(DonationService)
+@inject(TweetService)
 export class Signup {
 
-  firstName = 'Matthew';
-  lastName = 'North';
-  email = 'matthew@north.com';
-  password = 'secret';
+  firstName = '';
+  lastName = '';
+  email = '';
+  password = '';
 
-  constructor(ds) {
-    this.donationService = ds;
+  constructor(ts) {
+    this.tweetService = ts;
+  }
+
+  attached() {
+    setUpFormValidator(this.register.bind(this), null);
   }
 
   register(e) {
     this.showSignup = false;
-    this.donationService.register(this.firstName, this.lastName, this.email, this.password);
-    this.donationService.login(this.email, this.password);
+    this.tweetService.register(this.firstName, this.lastName, this.email, this.password);
+    this.tweetService.login(this.email, this.password);
   }
 }
+
+function setUpFormValidator(onSuccess, onFailure) {
+  $('#registerForm')
+      .form({
+        on: 'submit',
+        inline: true,
+        onSuccess: function(event) {
+          //prevents redirect
+          console.log('Successfully validated');
+          event.preventDefault();
+          onSuccess();
+        },
+        onFailure: function() {
+          console.log('Validation failed');
+          onFailure();
+          return false;
+        },
+        fields: {
+          firstname: {
+            identifier: 'firstname',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'First name must not be emtpy!'
+              }
+            ]
+          },
+          lastname: {
+            identifier: 'lastname',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Last name must not be emtpy!'
+              }
+            ]
+          },
+          mail: {
+            identifier: 'mail',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Mail must not be emtpy!'
+              },
+              {
+                type: 'email',
+                prompt: 'Not a valid Email!'
+              }
+            ]
+          },
+          password: {
+            identifier: 'password',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Password must not be emtpy!'
+              },
+              {
+                type: 'minLength[6]',
+                prompt: 'Password must have at least 6 characters!'
+              }
+            ]
+          }
+        }
+      })
+  ;
+}
+
+
