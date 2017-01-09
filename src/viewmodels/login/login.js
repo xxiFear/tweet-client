b2bcdc9432027706e63421164da5456ac79b5f1a
xxiFear <matthias.nord@hotmail.de>
2017-01-09 18:41:52 +0100

    Added validation to login form. This is not needed as passwords are validated upon registering but it prevents unnecessary api calls

--- src/viewmodels/login/login.js
+++ src/viewmodels/login/login.js
@@ -12,8 +12,60 @@ export class Login {
     this.prompt = '';
   }
 
+  attached() {
+    setUpFormValidator(this.login.bind(this), null);
+  }
+
   login(e) {
     console.log(`Trying to log in ${this.email}`);
     this.tweetService.login(this.email, this.password);
   }
+}
+
+function setUpFormValidator(onSuccess, onFailure) {
+  $('#loginForm')
+      .form({
+        on: 'submit',
+        inline: true,
+        onSuccess: function(event) {
+          //prevents redirect
+          console.log('Successfully validated');
+          event.preventDefault();
+          onSuccess();
+        },
+        onFailure: function() {
+          console.log('Validation failed');
+          onFailure();
+          return false;
+        },
+        fields: {
+          mail: {
+            identifier: 'mail',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Mail must not be emtpy!'
+              },
+              {
+                type: 'email',
+                prompt: 'Not a valid Email!'
+              }
+            ]
+          },
+          password: {
+            identifier: 'password',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Password must not be emtpy!'
+              },
+              {
+                type: 'minLength[6]',
+                prompt: 'Password must have at least 6 characters!'
+              }
+            ]
+          }
+        }
+      })
+  ;
 }
