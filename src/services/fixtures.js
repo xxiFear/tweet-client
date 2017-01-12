e0bb89c7a4dae5ae3245b51e8cc45b4b2f996580
xxiFear <matthias.nord@hotmail.de>
2017-01-12 20:04:49 +0100

    Changed fixtures to heroku

--- src/services/fixtures.js
+++ src/services/fixtures.js
@@ -1,4 +1,4 @@
 export default class Fixtures {
-  // baseUrl = 'http://localhost:4000';
-  baseUrl = 'https://afternoon-island-28462.herokuapp.com:4000';
+  baseUrl = 'http://localhost:4000';
+  // baseUrl = 'https://afternoon-island-28462.herokuapp.com:4000';
 }
